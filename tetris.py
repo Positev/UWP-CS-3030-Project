1e7c771dc18a0fcef4f85032d17c2975d8d344c2
Trevor Keegan <keegant@uwplatt.edu>
2021-04-27 14:32:41 -0500

    Adding statistics reporting

--- tetris.py
+++ tetris.py
@@ -1,20 +1,6 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 
-# Very simple tetris implementation
-#
-# Control keys:
-#       Down - Drop stone faster
-# Left/Right - Move stone
-#         Up - Rotate Stone clockwise
-#     Escape - Quit game
-#          P - Pause game
-#     Return - Instant drop
-#
-# Have fun!
-
-# NOTE: If you're looking for the old python2 version, see
-#       <https://gist.github.com/silvasur/565419/45a3ded61b993d1dd195a8a8688e7dc196b08de8>
 
 # Copyright (c) 2010 "Laria Carolin Chabowski"<me@laria.me>
 #
@@ -38,14 +24,43 @@
 
 from random import randrange as rand
 import pygame, sys, neat, os, numpy,time
-import pickle
+import threading
+import pickle, shutil
+
+from neat.math_util import mean, stdev
+from neat.six_util import itervalues, iterkeys
+
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
+import numpy as np
+from stat import S_IREAD, S_IRGRP, S_IROTH
+
+RUN_ID = input("Name this run: ")
+RUNS_PATH = os.path.join(os.getcwd(), 'Runs')
+if not os.path.exists(RUNS_PATH):
+    os.mkdir(RUNS_PATH)
+    
+RUN_PATH = os.path.join(RUNS_PATH, RUN_ID)
+if not os.path.exists(RUN_PATH):
+    os.mkdir(RUN_PATH)
+LOG_FILE = os.path.join(RUN_PATH, 'LOG.TXT')
+SCRIPT_NAME = 'tetris.py'
+CONFIG_NAME = 'NEATconfig.txt'
+HEADLESS = True
+
+shutil.copyfile(os.path.join(os.getcwd(), SCRIPT_NAME), os.path.join(RUN_PATH, SCRIPT_NAME))
+shutil.copyfile(os.path.join(os.getcwd(), CONFIG_NAME), os.path.join(RUN_PATH, CONFIG_NAME))
+
+os.chmod(os.path.join(RUN_PATH, SCRIPT_NAME), S_IREAD|S_IRGRP|S_IROTH)
+os.chmod(os.path.join(RUN_PATH, CONFIG_NAME), S_IREAD|S_IRGRP|S_IROTH)
+
 OUTPUT_FILE_PATH = "winner.pkl"
 INPUT_FILE_PATH = "winner.pkl"
 # The configuration
 cell_size = 18
 cols =      10
 rows =      22
-maxfps =    30
+maxfps =    20
 
 colors = [
 (0,   0,   0  ),
@@ -145,18 +160,22 @@ def calculate_fitness(score, board):
 
 class TetrisApp(object):
     def __init__(self):
-        pygame.init()
-        pygame.key.set_repeat(250,25)
+        self.gameover = False
+        self.paused = False
+        if not HEADLESS:
+            pygame.init()
+            pygame.key.set_repeat(250,25)
         self.width = cell_size*(cols+6)
         self.height = cell_size*rows
         self.rlim = cell_size*cols
         self.bground_grid = [[ 8 if x%2==y%2 else 0 for x in range(cols)] for y in range(rows)]
 
-        self.default_font =  pygame.font.Font(
-            pygame.font.get_default_font(), 12)
+        if not HEADLESS:
+            self.default_font =  pygame.font.Font(
+                pygame.font.get_default_font(), 12)
 
-        self.screen = pygame.display.set_mode((self.width, self.height))
-        pygame.event.set_blocked(pygame.MOUSEMOTION) # We do not need
+            self.screen = pygame.display.set_mode((self.width, self.height))
+            pygame.event.set_blocked(pygame.MOUSEMOTION) # We do not need
                                                      # mouse movement
                                                      # events, so we
                                                      # block them.
@@ -174,15 +193,25 @@ class TetrisApp(object):
                            (self.stone_x, self.stone_y)):
             self.gameover = True
 
+    def drop_timer(self, ):
+        self.drop(False)
+        threading.Timer(self.drop_time, self.drop_timer).start()
+
     def init_game(self):
         self.board = new_board()
         self.new_stone()
         self.level = 1
         self.score = 0
         self.lines = 0
-        pygame.time.set_timer(pygame.USEREVENT+1, 100)
+        self.drop_time = .01
+        if not HEADLESS:
+            pygame.time.set_timer(pygame.USEREVENT+1, self.drop_time)
+        else:
+            self.drop_timer()
 
     def disp_msg(self, msg, topleft):
+        if HEADLESS:
+            return
         x,y = topleft
         for line in msg.splitlines():
             self.screen.blit(
@@ -195,6 +224,8 @@ class TetrisApp(object):
             y+=14
 
     def center_msg(self, msg):
+        if HEADLESS:
+            return
         for i, line in enumerate(msg.splitlines()):
             msg_image =  self.default_font.render(line, False,
                 (255,255,255), (0,0,0))
@@ -208,6 +239,8 @@ class TetrisApp(object):
               self.height // 2-msgim_center_y+i*22))
 
     def draw_matrix(self, matrix, offset):
+        if HEADLESS:
+            return
         off_x, off_y  = offset
         for y, row in enumerate(matrix):
             for x, val in enumerate(row):
@@ -231,7 +264,9 @@ class TetrisApp(object):
             self.level += 1
             newdelay = 1000-50*(self.level-1)
             newdelay = 100 if newdelay < 100 else newdelay
-            pygame.time.set_timer(pygame.USEREVENT+1, newdelay  / 5)
+            self.drop_time = newdelay  / 5
+            if not HEADLESS:
+                pygame.time.set_timer(pygame.USEREVENT+1, self.drop_time)
 
     def move(self, delta_x):
         if not self.gameover and not self.paused:
@@ -245,8 +280,9 @@ class TetrisApp(object):
                                    (new_x, self.stone_y)):
                 self.stone_x = new_x
     def quit(self):
-        self.center_msg("Exiting...")
-        pygame.display.update()
+        if not HEADLESS:
+            self.center_msg("Exiting...")
+            pygame.display.update()
         sys.exit()
 
     def drop(self, manual):
@@ -311,14 +347,16 @@ class TetrisApp(object):
         self.gameover = False
         self.paused = False
 
-        dont_burn_my_cpu = pygame.time.Clock()
+        if not HEADLESS:
+            dont_burn_my_cpu = pygame.time.Clock()
         while 1:
-            self.screen.fill((0,0,0))
+            if not HEADLESS:
+                self.screen.fill((0,0,0))
             if self.gameover:
                 fitness = calculate_fitness(self.score, self.board)
                 print(fitness)
                 return fitness
-            else:
+            elif not HEADLESS:
                 pygame.draw.line(self.screen,(255,255,255),(self.rlim+1, 0),(self.rlim+1, self.height-1))
                 self.disp_msg("Next:", (self.rlim+cell_size, 2))
                 self.disp_msg("Score: %d\n\nLevel: %d\nLines: %d" % (self.score, self.level, self.lines), (self.rlim+cell_size, cell_size*5))
@@ -327,7 +365,8 @@ class TetrisApp(object):
                 self.draw_matrix(self.stone, (self.stone_x, self.stone_y))
                 self.draw_matrix(self.next_stone, (cols+1,2))
             
-            pygame.display.update()
+            if not HEADLESS:
+                pygame.display.update()
 
             stone = numpy.concatenate(self.stone)
             next_stone = numpy.concatenate(self.stone)
@@ -341,10 +380,94 @@ class TetrisApp(object):
             for i in range(len(options)):
                 if decision[i] > .5:
                     key_actions[options[i]]()
-                    break
 
-            self.drop(False)
-            dont_burn_my_cpu.tick(maxfps)
+
+            if not HEADLESS:
+                dont_burn_my_cpu.tick(maxfps)
+                for event in pygame.event.get():
+                    if event.type == pygame.USEREVENT+1:
+                        self.drop(False)
+                    elif event.type == pygame.QUIT:
+                        self.quit()
+
+class FileReporter(neat.reporting.BaseReporter):
+    """Uses `print` to output information about the run; an example reporter class."""
+
+    def print(self, msg):
+        with open(self.file_name, 'a+') as log_file:
+            log_file.write(str(msg) + '\n')
+    
+    def __init__(self, show_species_detail, file_name):
+        self.show_species_detail = show_species_detail
+        self.generation = None
+        self.generation_start_time = None
+        self.generation_times = []
+        self.num_extinctions = 0
+        self.file_name = file_name
+
+    def start_generation(self, generation):
+        self.generation = generation
+        self.print('\n ****** Running generation {0} ****** \n'.format(generation))
+        self.generation_start_time = time.time()
+
+    def end_generation(self, config, population, species_set):
+        ng = len(population)
+        ns = len(species_set.species)
+        if self.show_species_detail:
+            self.print('Population of {0:d} members in {1:d} species:'.format(ng, ns))
+            sids = list(iterkeys(species_set.species))
+            sids.sort()
+            self.print("   ID   age  size  fitness  adj fit  stag")
+            self.print("  ====  ===  ====  =======  =======  ====")
+            for sid in sids:
+                s = species_set.species[sid]
+                a = self.generation - s.created
+                n = len(s.members)
+                f = "--" if s.fitness is None else "{:.1f}".format(s.fitness)
+                af = "--" if s.adjusted_fitness is None else "{:.3f}".format(s.adjusted_fitness)
+                st = self.generation - s.last_improved
+                self.print(
+                    "  {: >4}  {: >3}  {: >4}  {: >7}  {: >7}  {: >4}".format(sid, a, n, f, af, st))
+        else:
+            self.print('Population of {0:d} members in {1:d} species'.format(ng, ns))
+
+        elapsed = time.time() - self.generation_start_time
+        self.generation_times.append(elapsed)
+        self.generation_times = self.generation_times[-10:]
+        average = sum(self.generation_times) / len(self.generation_times)
+        self.print('Total extinctions: {0:d}'.format(self.num_extinctions))
+        if len(self.generation_times) > 1:
+            self.print("Generation time: {0:.3f} sec ({1:.3f} average)".format(elapsed, average))
+        else:
+            self.print("Generation time: {0:.3f} sec".format(elapsed))
+
+    def post_evaluate(self, config, population, species, best_genome):
+        # pylint: disable=no-self-use
+        fitnesses = [c.fitness for c in itervalues(population)]
+        fit_mean = mean(fitnesses)
+        fit_std = stdev(fitnesses)
+        best_species_id = species.get_species_id(best_genome.key)
+        self.print('Population\'s average fitness: {0:3.5f} stdev: {1:3.5f}'.format(fit_mean, fit_std))
+        self.print(
+            'Best fitness: {0:3.5f} - size: {1!r} - species {2} - id {3}'.format(best_genome.fitness,
+                                                                                 best_genome.size(),
+                                                                                 best_species_id,
+                                                                                 best_genome.key))
+
+    def complete_extinction(self):
+        self.num_extinctions += 1
+        self.print('All species extinct.')
+
+    def found_solution(self, config, generation, best):
+        self.print('\nBest individual in generation {0} meets fitness threshold - complexity: {1!r}'.format(
+            self.generation, best.size()))
+
+    def species_stagnant(self, sid, species):
+        if self.show_species_detail:
+            self.print("\nSpecies {0} with {1} members is stagnated: removing it".format(sid, len(species.members)))
+
+    def info(self, msg):
+        self.print(msg)
 
 GEN = 0
 
@@ -370,6 +493,16 @@ def eval_genomes(genomes, config):
         App = TetrisApp()
         genome.fitness += App.run(net, genome)
         del App
+        
+    ids = []
+    fits = []
+    for i in range(len(genomes)):
+        ids.append(genomes[i][0])
+        fits.append(genomes[i][1].fitness)
+    
+    plt.scatter(ids, fits)
+    plt.savefig(os.path.join(RUN_PATH, f"Generation_{GEN}_scatter.png"))
+    plt.clf()
 if __name__ == '__main__':
     config_path = os.path.join(os.getcwd(), "NEATconfig.txt")
     config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, 
@@ -377,11 +510,11 @@ if __name__ == '__main__':
 
     population = neat.Population(config)
 
-    population.add_reporter(neat.StdOutReporter(True))
+    population.add_reporter(FileReporter(True, LOG_FILE))
     stats = neat.StatisticsReporter()
     population.add_reporter(stats)
 
-    winner = population.run(eval_genomes , 100)
+    winner = population.run(eval_genomes , 50)
 
     with open(OUTPUT_FILE_PATH, "wb") as f:
         pickle.dump(winner, f)
